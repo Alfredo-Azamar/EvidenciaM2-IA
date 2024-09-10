3ec6fd596c6d55c243dd18d79e8491d064182f64
Alfredo-Azamar <115026054+Alfredo-Azamar@users.noreply.github.com>
2024-09-10 00:37:38 -0600

    Add files via upload

--- MomentoRetro Pt2/evidencia2_M2_A01798100.py
+++ MomentoRetro Pt2/evidencia2_M2_A01798100.py
@@ -5,6 +5,9 @@
 
 # Importar librerías
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
@@ -17,9 +20,8 @@ y = data.target
 
 # Dividir el dataset en entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-# model = RandomForestClassifier(n_estimators=100, random_state=42)
-rfBase = RandomForestClassifier(random_state=42)
 
+# Definición de los hiperparámetros
 param_grid = {
     'n_estimators': [50, 100, 200],
     'max_depth':  [2, 10, 20, 30],
@@ -27,27 +29,40 @@ param_grid = {
     'min_samples_leaf': [1, 2, 4]
 }
 
-gridSearch = GridSearchCV(rfBase, param_grid, cv=5)
+# Crear el modelo base
+rfBase = RandomForestClassifier(random_state=42)
+gridSearch = GridSearchCV(rfBase, param_grid, cv=5) # 5-fold cross-validation
 
-# Entrenamiento
+# Entrenamiento del modelo
 gridSearch.fit(X_train, y_train)
 
+# Se obtienen los mejores parámetros y el mejor estimador
 bestParams = gridSearch.best_params_
 print(f"Mejores parámetros: {bestParams}")
 
+bestEstimator = gridSearch.best_estimator_
+print(f"Mejor estimador: {bestEstimator}")
+
 # Predicción
-bestRF = gridSearch.best_estimator_
-y_pred = bestRF.predict(X_test)
+y_pred = bestEstimator.predict(X_test)
 
 # Métricas
-# -- Accuracy --
+# -- Exactitud --
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Exactitud: {accuracy}")
 
 # -- Matriz de confusión --
-# confusion_matrix = confusion_matrix(y_test, y_pred)
-# print("Matriz de confusión:")
-# print(confusion_matrix)
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print("Matriz de confusión:")
+print(confusion_matrix)
+
+# Graficarlo
+plt.figure(figsize=(8, 6))
+sns.heatmap(confusion_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=data.target_names, yticklabels=data.target_names)
+plt.xlabel('Predicción')
+plt.ylabel('Actual')
+plt.title('Matriz de confusión - Random Forest')
+plt.show()
 
 # -- Reporte de clasificación --
 class_report = classification_report(y_test, y_pred)
